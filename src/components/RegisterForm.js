d0ef422ff7685231a80f12d8188507bfcf8605df
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-12-16 20:26:57 -0800

    Created the register component

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,11 +1,62 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function RegisterForm() {
-    return (
-        <div>
-            working
-        </div>
+    const [formState, setFormState] = useState({
+        username: "",
+        password: ""
+      });
+    const [errors, setErrors] = useState({
+        username: "",
+        password: ""
+      });
+
+      const inputChange = (e) => {
+
+          setFormState({
+            ...formState,
+              [e.target.name]: e.target.value
+          })
+      }
+
+        return (
+            <div>
+              <form>
+                <fieldset>
+                  <legend>
+                    <h1>Sign up</h1></legend>
+                  <label htmlFor="username" className="labels">Username: </label>
+                  <input
+                    placeholder="Create a Username"
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={formState.username}
+                    onChange={inputChange}
+                  />
+                  {errors.username.length > 0 ? (
+                    <p className="error">{errors.username}</p>
+                  ) : null}
+        
+        
+                  <label htmlFor="password" className="labels"> Password: </label>
+                  <input
+                    placeholder="Create a Password"
+                    type="text"
+                    id="password"
+                    name="password"
+                    value={formState.password}
+                    onChange={inputChange}
+                  />
+                  {/* <i onClick={togglePassword}>{eye}</i>
+                  {errors.password.length > 0 ? (
+                    <p className="error">{errors.password}</p>
+                  ) : null} */}
+                  <button type="submit">
+                    Sign Up
+                  </button>
+                </fieldset>
+              </form>
+            </div>
     )
 }
-
 export default RegisterForm
