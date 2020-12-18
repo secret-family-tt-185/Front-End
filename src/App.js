9ffdd80fb6696224e06716bfee48fb1a5c1424b4
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-12-17 19:22:14 -0800

    Created the axios with auth function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
 import Recipes from "./components/Recipes";
 import './App.css';
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./components/utils/PrivateRoute";
+
 
 
 
@@ -20,7 +22,7 @@ function App() {
       <Switch>
   <Route exact path="/" render={() => <LoginForm/>} /> 
   <Route path="/register" render={() => <RegisterForm/>} />
-  <Route path="/protected" component={Recipes} />
+  <PrivateRoute exact path="/protected" component={Recipes} />
       </Switch>
     </div>
     </Router>
