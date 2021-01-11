1e4a932fdf2c1da013b40b6421c56d9921819cef
Matthew Martin <zhintateo19924559@gmail.com>
2021-01-10 22:44:55 -0600

    Corrected the Token bug

--- src/utils/AxiosWithAuth.js
+++ src/utils/AxiosWithAuth.js
@@ -4,12 +4,12 @@ import axios from "axios"
 
 export const axiosWithAuth = () => {
 
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     
     return axios.create({
     baseURL: "https://secret-family.herokuapp.com", 
     headers: {
-        authorization: `${token} 1`
+        Authorization: `${token}`
     }
 })
 }
